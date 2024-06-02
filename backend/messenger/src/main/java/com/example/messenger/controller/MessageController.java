565c8f6b817b29156052a55cb69eb198bc1540e6
jeetpratap <jeetpratap@yahoo.com>
2024-06-02 10:57:42 +0000

    Set up Gitpod configuration and initialize project

---
+++ backend/messenger/src/main/java/com/example/messenger/controller/MessageController.java
@@ -0,0 +1,25 @@
+package com.example.messenger.controller;
+
+import com.example.messenger.model.Message;
+import com.example.messenger.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @GetMapping
+    public List<Message> getAllMessages() {
+        return messageRepository.findAll();
+    }
+
+    @PostMapping
+    public Message createMessage(@RequestBody Message message) {
+        return messageRepository.save(message);
+    }
+}
---
+++ backend/messenger/src/main/java/com/example/messenger/model/Message.java
@@ -0,0 +1,31 @@
+package com.example.messenger.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
